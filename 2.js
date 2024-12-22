86651de1fb30837a1861613f29ab5116b5c76ea4
duMix12 <konta6440@gmail.com>
2024-12-22 15:03:30 +0100

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,7 @@
+const quizData = [
+    {
+      question: "Thomas Stanley Holland - brytyjski aktor, któremu sławę przyniosła rola Petera Parkera w popularnej serii komiksów o człowieku-pająku zapoczątkowanej w 1962 roku. W którym filmie po raz pierwszy wcielił się w tę kultową rolę?",
+      options: ["Spider-Man (2002)", "Spider-Man 2 (2004)", "Spider-Man 3 (2007", "Kapitan Ameryka: Wojna bohaterów (2016)"],
+      answer: "Kapitan Ameryka: Wojna bohaterów (2016)"
+    },
+]
